5c237d2b1cfbdf9529cb9c4c97b7e08dca8c3bed
Petr Grinevich <7peter7@tut.by>
2020-12-16 21:54:14 +0300

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import './App.css';
-import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from './lib/apolloClient';
-import { DailyReportPage } from './DailyReportPage';
-import {  login } from './lib/user';
+import React from "react";
+import "./App.css";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "./lib/apolloClient";
+import { DailyReportPage } from "./DailyReportPage";
+import { login } from "./lib/user";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
-login('test1a@capmo.de', '123456qwerty');
+login("test1a@capmo.de", "123456qwerty");
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      default: "#EAEAEA",
+    },
+    primary: {
+      main: "#A2C617",
+    },
+    secondary: {
+      main: "#2C2C2C",
+    },
+  },
+});
 
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
-      <DailyReportPage 
-        projectId="a5b60f3a-8e94-11ea-be39-b300a4e40f96"
-        date="2020-11-08"
-      />
+      <ThemeProvider theme={theme}>
+        <DailyReportPage
+          projectId="a5b60f3a-8e94-11ea-be39-b300a4e40f96"
+          date="2020-11-08"
+        />
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
--- src/DailyReportPage/index.tsx
+++ src/DailyReportPage/index.tsx
@@ -1,7 +1,16 @@
 import React, { FC } from "react";
 import { OrderDirection } from "./api/getSpecialOccurrences";
 import SpecialOccurence from "./components/SpecialOccurence";
-import { Button, Container } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Container,
+  makeStyles,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import ErrorIcon from "@material-ui/icons/Error";
 import { useManageOccurrencies } from "./hooks/useManageOccurrencies";
 
 type DailyReportPageProps = {
@@ -9,10 +18,34 @@ type DailyReportPageProps = {
   date: string;
 };
 
+const useStyles = makeStyles({
+  wrapper: {
+    padding: "3rem 5rem",
+  },
+  header: {
+    display: "flex",
+    padding: "1rem 0",
+    marginBottom: "1rem",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  empty: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "2rem",
+    "& svg": {
+      marginRight: "0.5rem",
+    },
+  },
+});
+
 export const DailyReportPage: FC<DailyReportPageProps> = ({
   projectId,
   date,
 }) => {
+  const classNames = useStyles();
+
   const {
     data,
     create: handleCreate,
@@ -20,24 +53,51 @@ export const DailyReportPage: FC<DailyReportPageProps> = ({
     remove: handleRemove,
   } = useManageOccurrencies(date, projectId, OrderDirection["desc"]);
 
+  const renderForm = () => (
+    <form noValidate autoComplete="off">
+      {data?.result?.items.map((obj) => (
+        <SpecialOccurence
+          key={obj.id}
+          id={obj.id}
+          text={obj.description}
+          onEdit={handleUpdate}
+          onRemove={handleRemove}
+        />
+      ))}
+    </form>
+  );
+
+  const renderEmpty = () => (
+    <p className={classNames.empty}>
+      <ErrorIcon fontSize="small" color="error" /> No data yet
+    </p>
+  );
+
   return (
     <Container maxWidth="lg">
-      <h1>{date}</h1>
-      <div>
-        <h2>Special occurrencies</h2>
-        <Button onClick={handleCreate}>create</Button>
-        <form noValidate autoComplete="off">
-          {data?.result?.items.map((obj) => (
-            <SpecialOccurence
-              key={obj.id}
-              id={obj.id}
-              text={obj.description}
-              onEdit={handleUpdate}
-              onRemove={handleRemove}
-            />
-          ))}
-        </form>
-      </div>
+      <Paper className={classNames.wrapper} elevation={8}>
+        <Box
+          border={1}
+          borderTop={0}
+          borderRight={0}
+          borderLeft={0}
+          borderColor="grey.300"
+          className={classNames.header}
+        >
+          <Typography variant="h5" component="h1">
+            Special Occurrencies
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<AddIcon />}
+            onClick={handleCreate}
+          >
+            create
+          </Button>
+        </Box>
+        {data?.result.items.length ? renderForm() : renderEmpty()}
+      </Paper>
     </Container>
   );
 };
